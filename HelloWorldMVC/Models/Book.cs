41d09f2ebcb8da14d8e2e1e6d8622912bf176124
ihsan <42092819+ihsanfil@users.noreply.github.com>
2021-01-10 17:34:17 +0300

    book class created

---
+++ HelloWorldMVC/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorldMVC.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public DateTime PublishDate { get; set; }
+        public double Price { get; set; }
+        public int StockQuantity { get; set; }
+
+    }
+}
