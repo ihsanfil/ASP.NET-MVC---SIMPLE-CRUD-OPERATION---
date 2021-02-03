48723552ecfe2d42df47007aba5825aba6ffcbf7
ihsan <42092819+ihsanfil@users.noreply.github.com>
2021-02-03 17:37:20 +0300

    Validation işlemlerini yaptım, minumum-maksimum karakter sınırlaması getirildi.

--- HelloWorldMVC/Models/Book.cs
+++ HelloWorldMVC/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using HelloWorldMVC.Context;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,10 +10,16 @@ namespace HelloWorldMVC.Models
     public class Book
     {
         public int Id { get; set; }
+        [RegularExpression(@"^.{3,}$", ErrorMessage = "Minimum 3 karakter zorunludur !")]
+        [Required(ErrorMessage = "Başlık alanı girilmesi zorunludur !")]
         public string Title { get; set; }
+    
+        [Required(ErrorMessage = "Açıklama alanı girilmesi zorunludur !")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Minumum 3 Maksimum 200 karakter girilmesi zorunludur !")]
         public string Description { get; set; }
         public string Type { get; set; }
         public DateTime PublishDate { get; set; }
+        [Required(ErrorMessage = "Fiyat alanı girilmesi zorunludur !")]
         public double Price { get; set; }
         public int StockQuantity { get; set; }
         public Category Category { get; set; }
